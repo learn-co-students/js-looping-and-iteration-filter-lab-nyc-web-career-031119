66d94cf83fa2107c73cdaa24c7969a26d7d081ed
rowneee <roni.shabo@gmail.com>
2019-04-21 19:44:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(list, name) {
+  return list.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, partialName) {
+  let name = partialName.length;
+  return list.filter(function(driverName) {
+    return driverName.slice(0, name) === partialName;
+  });
+}
+
+function matchName(list, name) {
+  return list.filter(function(driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
