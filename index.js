5aaa907acf121660e8b6a28e4eddd91c7173ed08
Tommy Lau <tlau425@yahoo.com>
2019-04-22 15:54:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+function fuzzyMatch (list, firstLetters) {
+  let lengthOfName = firstLetters.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === firstLetters;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
