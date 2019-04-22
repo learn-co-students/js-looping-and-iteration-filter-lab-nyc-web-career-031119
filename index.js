14dbb210bad5d8853b4dc399a4830add571611fe
whosAle <abelgrave95@gmail.com>
2019-04-21 22:18:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(drivers, keyword) {
+  return drivers.filter(function (driver) {
+    return driver.toUpperCase() === keyword.toUpperCase();
+  });
+}
+
+function fuzzyMatch(drivers, keyword) {
+  return drivers.filter(function (driver) {
+    return driver.startsWith(keyword);
+  });
+}
+
+function matchName(drivers, keyword) {
+  return drivers.filter( driver => driver.name.toUpperCase() === keyword.toUpperCase());
+}
