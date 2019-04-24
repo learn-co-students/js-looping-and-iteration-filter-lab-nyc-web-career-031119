5f0c011f10f665f07d1b2715e097bb8481b3828a
David <davidbernier91@gmail.com>
2019-04-23 21:05:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file
+      
+function findMatching(drivers, name) {
+	return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase() ); 
+}
+
+// function findMatching (list, name) {
+//   return list.filter(function (driverName) {
+//     return driverName.toLowerCase() === name.toLowerCase();
+//   });
+// }
+
+
+
+
+
+ function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  	return 	list.filter(driverName => driverName.slice(0, lengthOfName) === partialName )
+ }
+
+
+//  function fuzzyMatch (list, partialName) {
+//   let lengthOfName = partialName.length;
+//   return list.filter(function (driverName) {
+//     return driverName.slice(0, lengthOfName) === partialName;
+//   });
+// }
+
+
+
+
+
+
+function matchName (list, name) {
+  return list.filter(driver => driver.name.toLowerCase() === name.toLowerCase() );
+  }
+
+// function matchName (list, name) {
+//   return list.filter(function (driver) {
+//     return driver.name.toLowerCase() === name.toLowerCase();
+//   });
+// }
